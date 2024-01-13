22906ba3a1a2d23c8f069cae83ae5492367c91d1
Jason Layton <16jalayt@gmail.com>
2024-01-12 22:46:58 -0600

    remove blog in footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -158,10 +158,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
+              /*{
                 label: 'Blog',
                 to: '/blog',
-              },
+              },*/
               {
                 label: 'GitHub',
                 href: githubUserLink,
